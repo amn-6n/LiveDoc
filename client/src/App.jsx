325fa5fde98a160d02e90aa45f712a31ff681dc6
AMN <amansingh2463@gmail.com>
2025-04-16 22:43:58 +0530

    Add demo component for room creation and joining functionality
    - Implemented a form for users to enter their username and room code.
    - Added functionality to store username in local storage.
    - Included a button to generate a room ID.
    - Set up form submission to handle room joining.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,15 +6,31 @@ import {
   Route,
 } from 'react-router-dom'
 import RoomForm from "./RoomForm"
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<RoomForm />} />
-        <Route path="/documents/:id" element={<TextEditor />} />
-      </Routes>
-    </Router>
+    <>
+      <div>
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            success: {
+              theme: {
+                primary: '#4aed88',
+              },
+            },
+          }}
+        ></Toaster>
+      </div>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<RoomForm />} />
+          <Route path="/documents/:id" element={<TextEditor />} />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
--- client/src/RoomForm.jsx
+++ client/src/RoomForm.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router';
-import { v4 as uuidV4} from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
+import toast from 'react-hot-toast'
 
 const RoomForm = () => {
   const [roomCode, setRoomCode] = useState('');
@@ -9,49 +10,70 @@ const RoomForm = () => {
   const generateRoomCode = () => {
     const uniqueCode = uuidV4()
     setRoomCode(uniqueCode);
+    toast.success('Successfully Room Id Generated');
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (username && roomCode) {
-       navigate(`/documents/${roomCode}`)
+      navigate(`/documents/${roomCode}`)
     } else {
-      alert('Please fill in all fields!');
+      toast.error('Room Id & Username Required');
     }
   };
 
   return (
-    <div>
-      <h2>Join or Create a Room</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="username">Username:</label>
-          <input
-            type="text"
-            id="username"
-            value={username}
-            onChange={(e) => {
+    <div className='homePageWrapper'>
+      <div className="formWrapper">
+
+        <div className="title">
+          <h1>Start Your </h1>
+          <h1 className='sub-title'>Collaboration 🤝</h1>
+        </div>
+
+        <form onSubmit={handleSubmit}>
+          <div className='inputGroup'>
+
+            {/* <label htmlFor="username">Username:</label> */}
+            <input
+              type="text"
+              className="inputBox"
+              id="username"
+              value={username}
+              onChange={(e) => {
                 setUsername(e.target.value);
-                localStorage.setItem('username', e.target.value); 
-            }}
-            placeholder="Enter your username"
-          />
-        </div>
-        <div>
-          <label htmlFor="roomCode">Room Code:</label>
-          <input
-            type="text"
-            id="roomCode"
-            value={roomCode}
-            onChange={(e) => setRoomCode(e.target.value)}
-            placeholder="Enter room code (optional)"
-          />
-          <button type="button" onClick={generateRoomCode}>
-            Create Room
-          </button>
-        </div>
-        <button type="submit">Join Room</button>
-      </form>
+                localStorage.setItem('username', e.target.value);
+              }}
+              placeholder="Enter Username"
+            />
+
+            {/* <label htmlFor="roomCode">Room Code:</label> */}
+            <input
+              type="text"
+              className="inputBox"
+              id="roomCode"
+              value={roomCode}
+              onChange={(e) => setRoomCode(e.target.value)}
+              placeholder="Enter Room Id (optional)"
+            />
+          </div>
+
+          <div className="btnGroup">
+            <button className="btn joinBtn" type="submit">Join Room</button>
+
+            <button className="btn createNewBtn" type="button" onClick={generateRoomCode}>
+              Generate Room Id
+            </button>
+          </div>
+        </form>
+
+      </div>
+      <footer>
+        <h4>
+          Built with ❤️ by &nbsp;
+          <a href="https://github.com/codersgyan">AMN SINGH</a>
+        </h4>
+      </footer>
     </div>
 
 
